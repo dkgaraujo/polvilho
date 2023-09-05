6e85bba1178def61a002301bc553d168e23b894c
Douglas Araujo <dkg.araujo@gmail.com>
2023-09-05 02:35:01 +0200

    minimalistic chrome cookie code

--- polvilho/main.swift
+++ polvilho/main.swift
@@ -6,6 +6,41 @@
 //
 
 import Foundation
+import SQLite3
 
-print("Hello, World!")
+// path to Chrome cookies
+let chromeCookiesDB = NSString("~/Library/Application Support/Google/Chrome/Default/Cookies")
+    .expandingTildeInPath
 
+// path to Mozilla cookies
+
+// path to Safari cookies
+
+// path to DuckDuckGo cookies
+
+func countChromeCookies() {
+    var db: OpaquePointer? = nil
+    if sqlite3_open(chromeCookiesDB, &db) == SQLITE_OK {
+        print("Chrome cookines... found!")
+        print("\n")
+        
+        let countCookiesQuery = "select count(*) from cookies"
+        var statement: OpaquePointer? = nil
+        
+        if
+            sqlite3_prepare_v2(db, countCookiesQuery, -1, &statement, nil) == SQLITE_OK {
+            if sqlite3_step(statement) == SQLITE_ROW {
+                let cookieCount = sqlite3_column_int(statement, 0)
+                print("Number of cookies found: \(cookieCount)")
+            } else {
+                print("Couldn't count :(")
+            }
+        } else {
+            print("Query preparation failed")
+        }
+    } else {
+        print("Failure...")
+    }
+}
+
+countChromeCookies()
