3abcc9e18df912da4b25a475bbbc9f7409d34dd9
Douglas Araujo <dkg.araujo@gmail.com>
2023-09-05 03:09:16 +0200

    Added deletion confirmation

--- polvilho/chrome.swift
+++ polvilho/chrome.swift
@@ -15,11 +15,10 @@ let chromeCookiesDB = NSString("~/Library/Application Support/Google/Chrome/Defa
 func countChromeCookies(delete: Bool) {
     var db: OpaquePointer? = nil
     if sqlite3_open(chromeCookiesDB, &db) == SQLITE_OK {
-        print("Chrome cookines... found!")
-        print("\n")
+        print("Chrome cookies... found!")
         
         let countCookiesQuery = "select count(*) from cookies"
-        let deleteCookiesQuery = "delete * from cookies;"
+        let deleteCookiesQuery = "delete from cookies;"
         var statement: OpaquePointer? = nil
         
         // Count cookies
@@ -43,6 +42,23 @@ func countChromeCookies(delete: Bool) {
             if sqlite3_prepare_v2(db, deleteCookiesQuery, -1, &statement, nil) == SQLITE_OK {
                 if sqlite3_step(statement) == SQLITE_DONE {
                     print("Chrome cookies deleted!")
+                    if
+                        sqlite3_prepare_v2(db, countCookiesQuery, -1, &statement, nil) == SQLITE_OK {
+                        
+                        if sqlite3_step(statement) == SQLITE_ROW {
+                            let cookieCount = sqlite3_column_int(statement, 0)
+                            if cookieCount == 0 {
+                                print("I can confirm all Chrome cookies are deleted.")
+                            } else {
+                                print("Weird, there are still \(cookieCount) cookie(s).")
+                            }
+                            
+                        } else {
+                            print("Couldn't count :(")
+                        }
+                    } else {
+                        print("Query preparation failed")
+                    }
                 } else {
                     print("Failed to delete the cookies.")
                 }
